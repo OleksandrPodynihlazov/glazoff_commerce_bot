50efe06bb082c46517fab3e20e6f454724308c58
Alex Glazoff <poduniglazov@gmail.com>
2024-08-05 18:59:13 +0100

    stable version of scrapping miltiple pages

--- main.py
+++ main.py
@@ -7,34 +7,39 @@ from bs4 import BeautifulSoup
 
 bot_token = telebot.TeleBot("7336652335:AAFbiaChNn64qJ9g8x5sSPpYlOeZuESPWBs")
 channel_name = "@glazoff_tg"
-page_counter =1
-def show_scrapped_data(page_counter):
-    page_counter +=1
-    glazoff_web_site = "https://glazoff.com/product-category/poslugy-dlya-internet-magazyniv/"+"page/"+str(page_counter)
-    return glazoff_web_site
+base_url = 'https://glazoff.com/product-category/poslugy-dlya-internet-magazyniv/'
+total_pages = 3
+def get_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return BeautifulSoup(response.text,'html.parser')
+    else:
+        print(f"Webpage downloading error {url}")
+        return None
+def scrape_page(base_url,total_pages):
+    results = []
 
-response = requests.get(show_scrapped_data())
-html_content = response.content
+    for page in range(1,total_pages+1):
+        url = f'{base_url}/page/{page}'
+        soup = get_data(url)
 
-def scrape_data(html_code):
-    soup = BeautifulSoup(html_code, "html.parser")
-    services_arr = []
-    services = soup.find("ul",class_="products columns-4")
-    services = services.find_all("li")
-    for service in services:
-        service_name_tag = service.find("h2")
-        service_name = service_name_tag.text.strip()
-        service_price_tag = service.find("span",class_="price")
-        if service_price_tag.find("ins"):
-            service_price_tag = service_price_tag.find("ins")
-            service_price_tag=service_price_tag.find("bdi")
-        service_price = service_price_tag.text.strip()
-        services_arr.append({
-        "Name":service_name,
-        "Price":service_price
-        })
-    return services_arr
-show_scrapped_data()
-show_scrapped_data()
-show_scrapped_data()
+        if soup:
+            services = soup.find("ul", class_="products columns-4")
+            services = services.find_all("li")
+            for service in services:
+                service_name_tag = service.find("h2")
+                service_name = service_name_tag.text.strip()
+                service_price_tag = service.find("span", class_="price")
+                if service_price_tag.find("ins"):
+                    service_price_tag = service_price_tag.find("ins")
+                    service_price_tag = service_price_tag.find("bdi")
+                service_price = service_price_tag.text.strip()
+                results.append({
+                    "Name": service_name,
+                    "Price": service_price
+                })
+    return results
+scraped_data = scrape_page(base_url,total_pages)
+for data in scraped_data:
+    print(data)
 bot_token.polling(none_stop=True)
