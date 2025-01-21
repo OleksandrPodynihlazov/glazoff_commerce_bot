bc7575d734a7bd18dbec70f55152f72784e56122
Oleksandr Podynihlazov <poduniglazov@gmail.com>
2025-01-21 02:30:18 +0000

    Delete current version directory

--- current version/bot.py
+++
@@ -1,6 +0,0 @@
-from handlers import TelegramBotHandler
-
-if __name__ == "__main__":
-    bot_handler = TelegramBotHandler()
-    bot_handler.register_handlers()
-    bot_handler.run()
--- current version/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MY_TELEGRAM_ID = os.getenv("MY_TELEGRAM_ID")
-SUPPORT_CONTACT = os.getenv("SUPPORT_CONTACT")
-BASE_URL = os.getenv("BASE_URL")
